4ca3b483ebb0aae44708dbc8481e27fde0e0f272
rabhatna <rabhatna@ucsc.edu>
2019-10-23 10:56:22 -0700

    Implemented function operations. Fixed typos

--- Contracts/Time-Token.sol
+++ Contracts/Time-Token.sol
@@ -6,7 +6,7 @@ contract TimeToken {
     string public standard = "Time Token v1.0";
     unit256 public totalSupply;
 
-    event Trasnfer(
+    event Transfer(
         address indexed _from,
         address indexed _to,
         uint256 _value
@@ -18,18 +18,47 @@ contract TimeToken {
         uint256 _value
     );
 
+    //Link addresses to TimeToken balances
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
+    //Contract constructor
     function TimeToken(uint256 _intialSupply) public {
-
+        balanceOf[msg.sender] = _intialSupply;
+        totalSupply = _intialSupply;
     }
 
+    //Sending tokens from one address to another
     function transfer(address _to, uint256 _value) public returns(bool success){
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        Transfer(msg.sender, _to, _value);
         return true;
     }
 
+    //Approve a valid transaction
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+
     function trasferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        
         return true;
     }
 
