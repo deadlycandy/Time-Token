51a235724ca995561a2aec87ac78c711bf8b2615
rabhatna <rabhatna@ucsc.edu>
2019-10-23 10:45:24 -0700

    Defined contract event parameters

--- Contracts/Time-Token.sol
+++ Contracts/Time-Token.sol
@@ -1,14 +1,22 @@
 pragma solidity ^0.4.2;
 
 contract TimeToken {
-    string public name;
-    string public symbol;
-    string public standard;
+    string public name = "Time Token";
+    string public symbol = "TT";
+    string public standard = "Time Token v1.0";
     unit256 public totalSupply;
 
-    event Trasnfer();
+    event Trasnfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
-    event Approval();
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
