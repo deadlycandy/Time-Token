b783972a2447f27ed6e7c384559c4e3813ec4244
rabhatna <rabhatna@ucsc.edu>
2019-10-23 13:22:01 -0700

    Implemented function operations. Comments

--- Contracts/TimeTokenSale.sol
+++ Contracts/TimeTokenSale.sol
@@ -10,19 +10,33 @@ contract TimeTokenSale {
 
     event Sell(address _buyer, uint _amount);
 
+    //Contstructor
     function TimeTokenSale(TimeToken _tokenContract, uint _tokenPrice) public  {
-
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
     }
 
     function multiply(uint x, uint y) internal pure returns(uint z) {
-
+        require(y == 0 || (z = x * y) / y == x);
     }
 
+    //Allowing user to purchase tokens
     function buyTokens(uint _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
+        tokensSold += _numberOfTokens;
+
+        Sell(msg.sender, _numberOfTokens);
     }
 
     function endSale() public {
-        
+        require(msg.sender == admin);
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
+
+        // Just transfer the balance to the admin
+        admin.transfer(address(this).balance);
     }
 }
